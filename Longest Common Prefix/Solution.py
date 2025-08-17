926ad834f2b69c8f7520b3fd36ed6abbedff8578
Abdullah Al Fahad <abdullahalfahad1999@gmail.com>
2025-08-17 21:39:58 +0600

    Longest Common Prefix

--- Longest Common Prefix/Solution.py
+++ Longest Common Prefix/Solution.py
@@ -1,5 +1,25 @@
-str = ["flower","flow","flight"]
-match = []
-for i in str:
-    for j in range(len(i)):
-        print(i,j)
+class Solution:
+    def longestCommonPrefix(self, strs):
+        min_len = min(len(word) for word in strs)
+        prefix = ""
+        flag = False
+        for i in range(min_len):
+            sample = strs[0][i]
+            for word in strs[1:]:
+                if word[i] != sample:
+                    flag = True
+                    break
+            if flag == True:
+                break
+                
+            prefix += sample
+        return prefix
+        
+
+
+
+
+strs = ["flower","flow","flight"]
+a =  Solution().longestCommonPrefix(strs)
+print(a)
+    
